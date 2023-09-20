a6f642ba7434c91d6f47c5887e0724e4b8561d4a
Indra Zulfi <me@indrazm.com>
2023-09-20 22:39:31 +0700

    refactor: Register UI

--- src/components/Auth/components/Register.jsx
+++ src/components/Auth/components/Register.jsx
@@ -1,62 +1,38 @@
-'use client'
+"use client";
 
 import { Button } from "@/components/SharedUI/Button";
 import { Input } from "@/components/SharedUI/Input";
 import { CheckBox } from "@/components/SharedUI/CheckBox";
-import { useState } from "react";
 import Link from "next/link";
+import { useRegister } from "../hooks/useRegister";
 
 export const Register = () => {
+  const { isLoading, registerData, handleChange, handleSubmitRegister } = useRegister();
 
-	const [registerData, setRegisterData] = useState({
-        name: "",
-        email: "",
-        password: ""
-    })
+  return (
+    <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+      <h1>Create Account for Party Everyday!</h1>
+      <div>
+        <Input value={registerData.name} label="Name" type="text" name="name" placeholder="Full Name" onChange={handleChange} />
+        <Input value={registerData.email} label="Your email" type="email" name="email" placeholder="your@email.com" onChange={handleChange} />
+        <Input value={registerData.password} label="Password" type="password" name="password" placeholder="Password" onChange={handleChange} />
+        <div className="flex gap-1">
+          <CheckBox id="" label="I accept the " type="checkbox" />
+          <Link href="#" className="link">
+            Terms and Conditions
+          </Link>
+        </div>
+        <Button isLoading={isLoading} onClick={handleSubmitRegister}>
+          Submit
+        </Button>
+      </div>
 
-    const handleChange = (e) => {
-        const { name, value } = e.target
-        setRegisterData({ ...registerData, [name]: value })
-    }
-
-    const handleSubmitRegister = async () => {
-        // const { name, email, password } = registerData
-	
-        // const res = await fetch("https://eventmakers-api.vercel.app/api/auth/register", {
-        //     method: "POST",
-		// 	headers: {
-        //         "Content-Type": "application/json",
-        //     },
-        //     body: JSON.stringify({ name, email, password }),
-        // });
-        // const data = await res.json();
-		// console.log(data)
-		console.log(registerData)
-    }	
-
-	return (
-			<div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-				<h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">Create Account for Party Everyday!</h1>
-				<div>
-					<Input label="Name" type="name" name="name" placeholder="Full Name" onChange={handleChange}/>	
-					<Input label="Your email" type="email" name="email" placeholder="youremail@.com" onChange={handleChange}/>
-					<Input label="Password" type="password" name="password" placeholder="Password" onChange={handleChange}/>
-					<div className="flex gap-1">
-						<CheckBox id="" label="I accept the " type="checkbox" />
-						<Link href="#" className="block text-sm font-medium text-blue-600 hover:underline dark:text-blue-500">
-							Terms and Conditions
-						</Link>
-					</div>
-
-					<Button onClick={handleSubmitRegister}>Submit</Button>
-				</div>
-
-				<label className="text-sm font-medium text-gray-900 dark:text-gray-300">
-					Already have account?
-					<Link href="#" className="text-blue-600 hover:underline dark:text-blue-500">
-						Login
-					</Link>
-				</label>
-			</div>
-	);
+      <label>
+        Already have account?
+        <Link href="#" className="link">
+          Login
+        </Link>
+      </label>
+    </div>
+  );
 };
--- src/components/SharedUI/Button.jsx
+++ src/components/SharedUI/Button.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 
-export const Button = ({ children }) => {
-	return (
-		<button
-			type="submit"
-			className=" inline-flex justify-center mb-5 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-		>
-			{children}
-		</button>
-	);
+export const Button = ({ isLoading, onClick, children }) => {
+  return (
+    <button
+      disabled={isLoading}
+      onClick={onClick}
+      type="submit"
+      className="disabled:bg-red-500 disabled:cursor-wait inline-flex justify-center mb-5 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+    >
+      {children}
+    </button>
+  );
 };
