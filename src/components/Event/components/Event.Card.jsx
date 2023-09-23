62e4e33294b6feea75ee560bd5379e258f149baf
hisyam <akbar_hisyam1@gmail.com>
2023-09-24 00:35:26 +0700

    style: styling card list

--- src/components/Event/components/Event.Card.jsx
+++ src/components/Event/components/Event.Card.jsx
@@ -1,28 +1,33 @@
 "use client";
 
+import { Button } from "@/components/SharedUI/Button";
 import Link from "next/link";
 import React from "react";
 
 export const EventCard = ({ events }) => {
-
-  return (
-    <div className="flex justify-between items-center h-screen ">
-      <div className="max-w-full m-auto p-4 grid grid-cols-3 gap-20 ">
-        {events.map((event) => {
-          return (
-            <Link key={event.id} href={`/events/${event.id}`}>
-              <div className="card w-[300px] py-3 px-4 flex-col items-start space-y-2 ">
-                <h4 className="font-bold text-large">{event.name}</h4>
-                <p className="font-semibold text-base leading-5 tracking-wide text-[#9E0620] ">{event.date}</p>
-                <p className="text-default-500">{event.location}</p>
-                <p>Participant : {event.participants.length}  </p>
-                <p>Penyelanggara : </p>
-                <h5 className="font-bold">{event.author.name}</h5>
-              </div>
-            </Link>
-          )
-        })}
-      </div>
-    </div >
-  )
-}
+	return (
+		<div className="max-w-screen-xl items-center justify-between mx-auto grid grid-cols-3 gap-6 mt-16">
+			{events.map((event) => {
+				return (
+					<Link key={event.id} href={`/events/${event.id}`}>
+						<div className="card">
+							<img
+								className="rounded-t-lg"
+								src="https://images.unsplash.com/photo-1514525253161-7a46d19cd819?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3474&q=80"
+								alt="event image"
+							/>
+							<div className="p-4 flex flex-col gap-3">
+								<h4 className="text-3xl font-bold text-gray-900 mb-2 text-center">{event.name}</h4>
+								<div className="flex w-full justify-between">
+									<p className="font-medium text-lg text-base text-neutral-700">{event.date}</p>
+									<p className="font-medium text-lg uppercase text-neutral-700">{event.location}</p>
+								</div>
+								<Button>Join with {event.participants.length} Other Participant</Button>
+							</div>
+						</div>
+					</Link>
+				);
+			})}
+		</div>
+	);
+};
